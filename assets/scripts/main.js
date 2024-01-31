fc1a08679b6af7611ed786c935bdbd27f68f4f1f
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-31 23:21:46 +0000

    added function getRandomHill, resized images

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,26 +1,35 @@
 /** 
  * Define the Constants so they can be used in the code
- * Constants for the modal
  * */
+
+
+
+//Constants for the modal
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("howToPlayBtn");
 const span = document.getElementsByClassName("close")[0];
 //Constants for the scoreboard
 const score = document.getElementsByClassName("score");
 const timer = document.getElementsByClassName("timer");
+const playBtn = document.getElementById("playNow");
+const resetBtn = document.getElementById("reset");
 //Constants in the game
-const moleHills = document.getElementsByClassName("moleHill");
-const moles = document.getElementsByClassName("mole");
-const plants = document.getElementsByClassName("plant");
+const molehills = [...document.querySelectorAll(".molehill")]
+const moles = document.querySelectorAll(".mole");
+const plants = document.querySelectorAll(".plant");
 
 
 // Define the variables
+let result = 0;
+let gameScore = 0;
+let gameTimer = 60;
+let gameRunning = false;
 
 
 /**
  * How to Play Modal
  * Open the modal with button click
- * */
+ **/
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -35,11 +44,23 @@ window.onclick = function (event) {
   }
 }
 
+
+
 /**
  * Event listeners for:
  * starting the game when play button clicked
  * Stop and reset the game when reset button is clicked
  */
+
+playBtn.onclick = function(){
+  alert("click");
+  console.log("click");
+}
+
+resetBtn.onclick = function() {
+  alert("reset");
+  console.log("reset");
+}
 
 
 /**
@@ -52,9 +73,47 @@ window.onclick = function (event) {
  * movePlant()
  */
 
+function playGame() {
+  gameRunning = true;
+  gameScore = 0;
+  gameTimer = 60;
+  //moveMole()
+  //movePlant()
+}
+
 /**
- * getRandomHill() selects a mole hill at random and places the mole in it
+ * function getRandomHill() selects a mole hill at random and places the mole in it
+ * only if game running
  */
+
+
+ function getRandomHill() {
+    if(gameTimer <=0) {
+      gameRunning = false;
+      return;
+    }
+
+    molehills.forEach(molehill => {
+      molehill.classList.remove("mole");
+      molehill.classList.remove("plant");
+    });
+
+    let randomHill = molehills[Math.floor(Math.random() * 9)];
+    randomHill.classList.add("mole");
+
+    let randomPlant = molehills[Math.floor(Math.random() * 9)]
+    randomPlant.classList.add("plant");
+
+    setTimeout(() => {
+      if(gameTimer <= 0) {
+        gameRunning = false;
+        return;
+      }
+    })
+
+}
+getRandomHill()
+
 
 /**
  * function moveMole(){                sets an interval of 1 second between mole moving between hills using getRandomHill()
@@ -62,15 +121,27 @@ window.onclick = function (event) {
  * moleTimer = setInterval(getRandomHill, 1000)}
  */
 
+
+
+
+/**
+ * function getRandomPlant() places a plant in a random tile similar to what getRandomHill does for the mole 
+ * --??? adding this into function getRandomHill
+ */
+
+
 /**
  * function movePlant() {             sets an interval of 1.5 seconds between plant moving using getRandomHill()
  * let plantTimer = null;
- * plantTimer = setInterval(getRandomTile, 1500)}
+ * plantTimer = setInterval(getRandomPlant, 1500)}
  */
+
+
 
 /**
  * function checkMoleHillEmpty()  moles and plants can't appear in the same molehill
  */
+
 
 /**
  * function updateTimer()
@@ -89,3 +160,5 @@ window.onclick = function (event) {
  * clears moles and plants from molehills
  * add pop-up with message informing of score
  */
+
+// module.exports = {}
--- assets/scripts/main.test.js
+++ assets/scripts/main.test.js
@@ -1,10 +1,16 @@
+const { addSum } = require("./main");
 
 
 beforeAll(() => {
     let fs = require("fs");
-    let fileContents = fs.readFileSync("play.html", "utf-8");
+    let fileContents = fs.readFileSync("play-game.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
 })
 
+describe("", () => {
+    test("", () => {
+        expect().toBe();
+    });
+})
